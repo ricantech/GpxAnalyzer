83cd7bbeba279764b54690e76d63155ce6446318
Michal Rican <Michal.Rican@4finance.com>
2019-03-15 14:38:52 +0100

    Remove NodeRepository as it's not needed

--- src/main/java/rican/task/data/gpxAnalyzer/repository/NodeRepository.java
+++
@@ -1,7 +0,0 @@
-package rican.task.data.gpxAnalyzer.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import rican.task.data.gpxAnalyzer.model.Node;
-
-public interface NodeRepository extends CrudRepository<Node, Long> {
-}
